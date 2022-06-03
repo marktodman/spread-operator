5537262b9207be251652c39b1cac94d3c18479cb
Mark Todman <93592366+marktodman@users.noreply.github.com>
2022-06-03 17:11:20 +0200

    Initial commit

---
+++ the-spread-operator.js
@@ -0,0 +1,12 @@
+/**
+ * To run this file in Gitpod, use the 
+ * command node the-spread-operator.js in the terminal
+ */
+
+// No spread operator
+
+// Copying an array
+
+// Copying an object
+
+// Copying only part of an array/object
